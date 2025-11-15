9b36d4088b8d99ae28a57963f9aa9de7f3c5a1df
Joshua Sarcol <jjsnippets@gmail.com>
2025-11-15 13:32:16 +0800

    cleanup

--- LE6/Team1_LE6-1.asm
+++ LE6/Team1_LE6-1.asm
@@ -1,6 +1,6 @@
 ; CPE 3104 - MICROPROCESSORS
 ; Group 4   TTh 4:30 PM - 7:30 PM LBCEAC2 TC
-; Ayabe, Kaishu; Sarcol, Joshua           BS-CpE 3        2025/10/29
+; Ayabe, Kaishu; Sarcol, Joshua           BS-CpE 3        2025/11/13
 ; Laboratory Exercise #6 | Parallel I/O Devices Interfacing
 
 ; Activity #1
@@ -9,23 +9,23 @@
 ; AND DATA_CTRL procedures.
 
 ORG 100H
-    DATAB EQU 0F0H      ; Data bus of LCD
-    CNTRL EQU 0F2H      ; LCD Control signals
-                        ; ---- --x- E
-                        ; ---- ---x RS       
-    COM_REG EQU 0F6H    ; 8255 Command Register
-                        ; 10000 000 (80H)
-                        ; 1---- --- Command Group A
-                        ; -00-- --- Mode 0 for PORTA
-                        ; ---0- --- Output for PORTA
-                        ; ----0 --- Output for PORTC7-4 (unused)
-                        ; ----- 0-- Mode 1 for PORTB
-                        ; ----- -0- Output for PORTB
-                        ; ----- --0 Output for PORTC3-0 (unused)
 
 .data
-STRNG DB "HELLO!"
-LEN EQU $ - STRNG    
+    DATAB EQU 0F0H          ; Data bus of LCD
+    CNTRL EQU 0F2H          ; LCD Control signals
+                            ; ---- --x- E
+                            ; ---- ---x RS       
+    COM_REG EQU 0F6H        ; 8255 Command Register
+                            ; 10000 000 (80H)
+                            ; 1---- --- Command Group A
+                            ; -00-- --- Mode 0 for PORTA
+                            ; ---0- --- Output for PORTA
+                            ; ----0 --- Output for PORTC7-4 (unused)
+                            ; ----- 0-- Mode 1 for PORTB
+                            ; ----- -0- Output for PORTB
+                            ; ----- --0 Output for PORTC3-0 (unused)
+    STRNG DB "HELLO!"
+    LEN EQU $ - STRNG    
     
 .code
     ; Program the 8255
@@ -61,78 +61,78 @@ HLT
 INIT_LCD:
     CALL delay
       
-    MOV AL, 038H        ; 0011 1000 (38H)
-                        ; 001? ?0xx Function Set
-                        ; ---1 ---- 8-bit data transfer
-                        ; ---- 1--- Dual line display
-    CALL INST_CTRL      ; ---- -0-- 5x8 Font size
+    MOV AL, 038H            ; 0011 1000 (38H)
+                            ; 001? ?0xx Function Set
+                            ; ---1 ---- 8-bit data transfer
+                            ; ---- 1--- Dual line display
+    CALL INST_CTRL          ; ---- -0-- 5x8 Font size
         
-    MOV AL, 08H         ; 0000 1000 (08H)
-                        ; 0000 1??? Display ON/OFF
-                        ; ---- -0-- Entire display off
-                        ; ---- --0- Cursor off
-    CALL INST_CTRL      ; ---- ---0 Cursor blinking off
+    MOV AL, 08H             ; 0000 1000 (08H)
+                            ; 0000 1??? Display ON/OFF
+                            ; ---- -0-- Entire display off
+                            ; ---- --0- Cursor off
+    CALL INST_CTRL          ; ---- ---0 Cursor blinking off
     
-    MOV AL, 01H         ; 0000 0001 (01H)
-    CALL INST_CTRL      ; 0000 0001 Clear Display
+    MOV AL, 01H             ; 0000 0001 (01H)
+    CALL INST_CTRL          ; 0000 0001 Clear Display
     
-    MOV AL, 06H         ; 0000 0110 (06H)
-                        ; 0000 01?? Entry Mode Set
-                        ; ---- --1- Increment / Move right
-    CALL INST_CTRL      ; ---- ---0 No shifting
+    MOV AL, 06H             ; 0000 0110 (06H)
+                            ; 0000 01?? Entry Mode Set
+                            ; ---- --1- Increment / Move right
+    CALL INST_CTRL          ; ---- ---0 No shifting
     
-    MOV AL, 0CH         ; 0000 1100 (0CH)
-                        ; 0000 1??? Display ON/OFF
-                        ; ---- -1-- Entire display on
-                        ; ---- --0- Cursor off
-    CALL INST_CTRL      ; ---- ---0 Cursor blinking off
+    MOV AL, 0CH             ; 0000 1100 (0CH)
+                            ; 0000 1??? Display ON/OFF
+                            ; ---- -1-- Entire display on
+                            ; ---- --0- Cursor off
+    CALL INST_CTRL          ; ---- ---0 Cursor blinking off
 RET
 
 ; LCD 8-bit instruction function
 ; AX - Instruction to be moved (will be clobbered)
 INST_CTRL:
-    PUSH DX             ; Housekeeping
+    PUSH DX                 ; Housekeeping
     
-    MOV DX, DATAB       ; Instruction to LCD data bus
+    MOV DX, DATAB           ; Instruction to LCD data bus
     OUT DX, AL
     
-    MOV DX, CNTRL       ; Set control pins
-    MOV AL, 02H         ; Instruction Register
-    OUT DX, AL          ; E = 1, RS = 0 
+    MOV DX, CNTRL           ; Set control pins
+    MOV AL, 02H             ; Instruction Register
+    OUT DX, AL              ; E = 1, RS = 0 
     
-    CALL delay          ; Wait before next instruction
+    CALL delay              ; Wait before next instruction
     
-    MOV AL, 00H         ; Not leave E always HIGH
+    MOV AL, 00H             ; Not leave E always HIGH
     OUT DX, AL
     
-    POP DX              ; Housekeeping
+    POP DX                  ; Housekeeping
 RET
 
 ; LCD 8-bit data function
 ; AX - Data to be moved (will be clobbered)
 DATA_CTRL:
-    PUSH DX             ; Housekeeping
+    PUSH DX                 ; Housekeeping
     
-    MOV DX, DATAB       ; To LCD data bus
+    MOV DX, DATAB           ; To LCD data bus
     OUT DX, AL
     
-    MOV DX, CNTRL       ; Set control pins
-    MOV AL, 03H         ; Data Register
-    OUT DX, AL          ; E = 1, RS = 1 
+    MOV DX, CNTRL           ; Set control pins
+    MOV AL, 03H             ; Data Register
+    OUT DX, AL              ; E = 1, RS = 1 
 
-    CALL delay          ; Wait before next instruction
+    CALL delay              ; Wait before next instruction
     
-    MOV AL, 01H         ; Not leave E always HIGH
+    MOV AL, 01H             ; Not leave E always HIGH
     OUT DX, AL
         
-    POP DX              ; Housekeeping
+    POP DX                  ; Housekeeping
 RET
 
 ; Artificial delay
 ; Should really check for BF (with interrupts)
 delay:
     PUSH BX
-    MOV BX, 0ACAH
+    MOV BX, 00FAH
     lp_:
     DEC BX
     NOP
